0923d0194e07b13f23f064116fe11162b50f872e
Александр <alexudalovvvvv@outlook.com>
2018-09-25 22:57:37 +0300

    Simple JWT authorization.

---
+++ RiseAndWalk-Server/Entities/AlarmsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RiseAndWalk_Server.Entities
+{
+    public class AlarmsDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=alarms_database;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ RiseAndWalk-Server/Entities/IdentyDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RiseAndWalk_Server.Entities
+{
+    public class IdentyDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=identy_database;Trusted_Connection=True;");
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseMySQL(GetConnectionString());
+        //}
+
+        private static string GetConnectionString()
+        {
+            const string databaseName = "webapijwt";
+            const string databaseUser = "root";
+            const string databasePass = "1";
+            
+            return $"Server=localhost;" +
+                   $"database={databaseName};" +
+                   $"uid={databaseUser};" +
+                   $"pwd={databasePass};" +
+                   $"pooling=true;";
+        }
+    }
+}
